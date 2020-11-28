832ca949861b037f3f41017363a0995ff4e9c2ee
Remigiusz Klarkowski <r.klarek@gmail.com>
2020-11-28 21:09:50 +0100

    Zegar zawsze aktualny

--- alarm.js
+++ alarm.js
@@ -2,7 +2,7 @@ var myVar;
 var schemat_polewania = {image: ["images/mieszek.png", "images/Pirzmi.png", "images/Remas.png", "images/Rosja.png", "images/Slawko.png"]};
 var losowanie;
 var intervalInSeconds = 90*60;
-var startinterval, y1, y2;
+var startinterval, y1, y2, startczas, roznicaczasu;
 
 
 function withZero(num){
@@ -39,6 +39,29 @@ function myTimer() {
   }
   document.getElementById("timer").innerHTML = format(intervalInSeconds);
 }
+function newTimer() {
+  roznicaczasu =  Date.now() - startczas;
+  if(startinterval - Math.floor(roznicaczasu/1000) > 0) {
+      intervalInSeconds-=0.1;
+      var size= document.getElementsByClassName("flex-container")[0].style.backgroundSize;
+      y2 = roznicaczasu*100/1000/startinterval;
+      y1 = 0.8*y2;
+      document.getElementsByClassName("flex-container")[0].style.backgroundSize = "100% "+y1+"%, 100% "+y2+"%";
+      // console.log("size", size);
+      if(intervalInSeconds < 0){
+        intervalInSeconds = 0;
+      }
+  } else {      
+    clearInterval(myVar);
+    var audio = document.getElementById("audio"); 
+    audio.play();
+    document.getElementById("button").style.visibility = "visible";
+    document.getElementById("text").style.visibility="visible";
+    document.getElementById("image").src = schemat_polewania.image[losowanie];
+    document.getElementById("image").style.visibility="visible";
+  }
+  document.getElementById("timer").innerHTML = format(startinterval - Math.floor(roznicaczasu/1000));
+}
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -47,9 +70,10 @@ function getRandomInt(min, max) {
 }
 
 function start() {
+  startczas = Date.now();
   losowanie = getRandomInt(0, 5);
-    myVar = setInterval(myTimer, 100);
-    intervalInSeconds = 90*60;
+    myVar = setInterval(newTimer, 100);
+    intervalInSeconds = 10;
     startinterval = intervalInSeconds.valueOf();
     document.getElementById("button").style.visibility = "hidden";
     document.getElementById("image").style.visibility="hidden";
